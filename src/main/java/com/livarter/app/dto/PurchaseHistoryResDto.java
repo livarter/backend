eabccbdfbd3184e382a94cb313b5f8aa14e20df4
skybluelion <brotherjun96@gmail.com>
2024-01-24 09:10:40 +0900

    feat: import 및 중복 dependency 수정

--- src/main/java/com/livarter/app/dto/PurchaseHistoryResDto.java
+++ src/main/java/com/livarter/app/dto/PurchaseHistoryResDto.java
@@ -1,14 +1,14 @@
 package com.livarter.app.dto;
 
-import com.livarter.app.domain.PurchaseHistory;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 /**
  * @author : 변형준
